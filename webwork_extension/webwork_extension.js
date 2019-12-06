ce67a72040af82110fd3cb7f2163cac202fc3319
oToToT <ty1208chiang@gmail.com>
2019-12-06 15:04:31 +0800

    use queryselector to append child

--- webwork_extension/webwork_extension.js
+++ webwork_extension/webwork_extension.js
@@ -1,30 +1,33 @@
-function getGrade(html_text){
-    var m = {};
-    let d = new DOMParser();
-    let doc = d.parseFromString(html_text, 'text/html');
-    let nodes = doc.querySelectorAll("#grades_table tr:not([class=grades-course-total])");
-    nodes.forEach(function(ele) {
-        let e = ele.getElementsByTagName('td');
-        if (e.length) m[e[0].innerText]=e[1].innerText;
-    })
-    return m;
-}
-let grades_html = await (await fetch("grades/")).text();
-let map = getGrade(grades_html);
-let text = document.body.textContent;
-let target = "open";
-let GG = "will";
-for (let i = 0; i < text.length - target.length; i ++) {
-    if (text.substr(i, GG.length) == GG) i += 20;
-    if (text.substr(i, target.length) == target) {
-        var j = i - 2;
-        while (text[j] != ' ') j --;
-        var homeworkID = text.substr(j + 1, i - j - 2);
-        if (homeworkID in map) {
-            let re = new RegExp(">" + homeworkID + "<", 'g');
-            document.body.innerHTML = document.body.innerHTML.replace(re, 
-                                                                      ">" + homeworkID + "</a><a = style=\"color:DodgerBlue;\">  " + map[homeworkID] + "<"
-                                                                     );
-        }
+(async function(){
+    function getGrade(html_text){
+        var m = {};
+        let d = new DOMParser();
+        let doc = d.parseFromString(html_text, 'text/html');
+        let nodes = doc.querySelectorAll("#grades_table tr:not([class=grades-course-total])");
+        nodes.forEach(function(ele) {
+            let e = ele.getElementsByTagName('td');
+            if (e.length) m[e[0].innerText]=e[1].innerText;
+        })
+        return m;
     }
-}
+    let grades_html = await (await fetch("grades/")).text();
+    let map = getGrade(grades_html);
+    document.querySelectorAll('a[class=set-id-tooltip]').forEach(function(ele) {
+        // to hide score in closed problems, please uncomment the statement below
+        // if (ele.parentNode.parentNode.innerText.includes('closed')) return;
+        let key = ele.innerText;
+        let span = document.createElement("span");
+        span.innerText = ` ${map[key]}`;
+        switch (map[key]) {
+            case '100%':
+                span.style.color = '#008000';
+                break;
+            case '0%':
+                span.style.color = '#ff0000';
+                break;
+            default:
+                span.style.color = '#1e90ff';
+        }
+        ele.parentNode.appendChild(span);
+    });
+})();
